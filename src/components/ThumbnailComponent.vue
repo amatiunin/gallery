42cd2f1bd8a1d00028c782483d071c137146e46b
Anton Matiunin <anton.matiunin@onix-systems.com>
2024-10-14 18:01:52 +0300

    preparing aws

---
+++ src/components/ThumbnailComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="thumbnail">
+    <img
+        :src="src"
+        :alt="`Thumbnail ${index}`"
+        @click="handleClick"
+        class="thumbnail-image"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+    index: {
+      type: Number,
+      required: true,
+    },
+    onClick: {
+      type: Function,
+      required: true,
+    },
+  },
+  methods: {
+    handleClick() {
+      this.onClick(this.index);
+    },
+  },
+}
+</script>
